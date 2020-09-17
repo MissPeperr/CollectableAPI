761f0e2c515e86e0ed23e540984dd6a79a31a0fb
Madi Peper <madisonpeper@gmail.com>
2020-09-17 13:22:41 -0500

    Actually fixed the fixtures this time; Forgot to add the foreign key to collectable for collection;

--- collectableapp/models/collectable.py
+++ collectableapp/models/collectable.py
@@ -1,13 +1,16 @@
 """Module for Collectable"""
 from django.db import models
+from collectableapp.models import Collection
 
 class Collectable(models.Model):
     """ Model class for Collectable"""
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=255)
-    boughtPrice = models.FloatField()
-    soldPrice = models.FloatField()
+    bought_price = models.FloatField()
+    sold_price = models.FloatField(null=True)
     imageURL = models.URLField()
+    is_sold = models.BooleanField(default=False)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE, related_name="collection")
 
     class Meta:
         verbose_name = ("collectable")
